2f63468c50655bf75738a6f16e7f475efcbc2504
Jacobtread <jacobtread@gmail.com>
2024-08-23 21:31:30 +1200

    feat: command line args and automatic office path resolution

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,37 @@ use axum::{
 };
 use axum_typed_multipart::{FieldData, TryFromMultipart, TypedMultipart};
 use bytes::Bytes;
+use clap::Parser;
 use error::DynHttpError;
 use libreofficekit::{DocUrl, Office, OfficeError, OfficeOptionalFeatures};
 use rand::{distributions::Alphanumeric, Rng};
 use serde::Serialize;
 use std::env::temp_dir;
 use tokio::sync::{mpsc, oneshot};
-use tracing::{debug, error};
+use tracing::{debug, error, level_filters::LevelFilter};
 use tracing_subscriber::EnvFilter;
 
 mod error;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the office installation (Omit to determine automatically)
+    #[arg(long)]
+    office_path: Option<String>,
+
+    /// Port to bind the server to, defaults to 8080
+    #[arg(long)]
+    port: Option<u16>,
+
+    /// Host to bind the server to, defaults to 0.0.0.0
+    #[arg(long)]
+    host: Option<String>,
+
+    /// Logging level to use
+    #[arg(long, default_value_t = LevelFilter::INFO)]
+    logging: LevelFilter,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -39,9 +60,40 @@ async fn main() -> anyhow::Result<()> {
     // use that subscriber to process traces emitted after this point
     tracing::subscriber::set_global_default(subscriber)?;
 
-    let office_path =
-        std::env::var("LIBREOFFICE_SDK_PATH").context("missing LIBREOFFICE_SDK_PATH")?;
-    let server_address = std::env::var("SERVER_ADDRESS").context("missing SERVER_ADDRESS")?;
+    let args = Args::parse();
+
+    let mut office_path: Option<String> = args.office_path;
+
+    // Try loading office path from environment variables
+    if office_path.is_none() {
+        office_path = std::env::var("LIBREOFFICE_SDK_PATH").ok()
+    }
+
+    // Try determine default office path
+    if office_path.is_none() {
+        office_path = Office::find_install_path().map(|value| value.to_string());
+    }
+
+    // Check a path was provided
+    let office_path = match office_path {
+        Some(value) => value,
+        None => {
+            error!("no office install path provided, cannot start server");
+            panic!();
+        }
+    };
+
+    debug!("using libreoffice install from: {office_path}");
+
+    // Determine the address to run the server on
+    let server_address = if args.host.is_some() || args.port.is_some() {
+        let host = args.host.unwrap_or_else(|| "0.0.0.0".to_string());
+        let port = args.port.unwrap_or(8080);
+
+        format!("{host}:{port}")
+    } else {
+        std::env::var("SERVER_ADDRESS").context("missing SERVER_ADDRESS")?
+    };
 
     // Create office access
     let office_handle = create_office_runner(office_path);
@@ -58,6 +110,8 @@ async fn main() -> anyhow::Result<()> {
     let listener = tokio::net::TcpListener::bind(&server_address)
         .await
         .context("failed to bind http server")?;
+
+    debug!("server started on: {server_address}");
 
     // Serve the app from the listener
     axum::serve(listener, app)
@@ -126,8 +180,6 @@ fn office_runner(path: String, mut rx: mpsc::Receiver<OfficeMsg>) -> anyhow::Res
     let temp_out_path = temp_out
         .to_str()
         .context("failed to create temp out path")?;
-
-    debug!(%temp_in_path, %temp_out_path);
 
     // Create office type safe paths
     let input_url = DocUrl::local_as_abs(temp_in_path).context("failed to create input url")?;
